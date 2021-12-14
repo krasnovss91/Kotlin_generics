1b5dff7f755da97928b70d5c9260f2b76ebe1773
krasnovss91 <krasnovstanislav91@gmail.com>
2021-12-14 14:34:49 +0400

    descriptors_3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,37 @@
-interface Desctiptor<T>{
+interface Desctiptor<T> {
     fun getDescription(description: T)
 }
 
-class BookDescriptor: Desctiptor<Book>{
+class BookDescriptor : Desctiptor<Book> {
     override fun getDescription(description: Book) {
-        println(description.data)
+        println(description.data + " " + description.author)
     }
 }
 
-class NewspaperDescriptor: Desctiptor<Newspaper>{
+class NewspaperDescriptor : Desctiptor<Newspaper> {
     override fun getDescription(description: Newspaper) {
-        println(description.data)
+        println(description.data + " " + description.number)
     }
 }
 
-class VideoDescriptor: Desctiptor<Video>{
+class VideoDescriptor : Desctiptor<Video> {
     override fun getDescription(description: Video) {
-        println(description.data)
+        println(description.data + " " + description.duration)
     }
 
 }
 
 fun main(args: Array<String>) {
     val bookDescriptor = BookDescriptor()
-    val book = Book("book_1","author_1")
+    val book = Book("book_1", "author_1")
     bookDescriptor.getDescription(book)
 
     val newspaperDescriptor = NewspaperDescriptor()
-    val newspaper = Newspaper("newspaper_1",11)
+    val newspaper = Newspaper("newspaper_1", 11)
     newspaperDescriptor.getDescription(newspaper)
 
     val videoDescriptor = VideoDescriptor()
-    val video = Video("video_1",25)
+    val video = Video("video_1", 25)
     videoDescriptor.getDescription(video)
 
 }
